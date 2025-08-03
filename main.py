a7b49e2381abc3d4de48390c27e8f88fafde7723
SnehChaudhari <164089738+SnehChaudhari@users.noreply.github.com>
2025-08-04 02:33:37 +1200

    v0.4 fixed game window
    eliminated visual glitches caused by resizing the game window, by getting rid of the ability to resize the game window altogether.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ screen_width = 1280
 screen_height = 720
 
 # makes the window resizable (with the default screen resolution)
-screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 # sets a name at the top of the window
 pygame.display.set_caption("Plastic Pollution Tetris")
@@ -20,14 +20,14 @@ background = pygame.transform.scale(background, (screen_width, screen_height))
 # main game loop
 running = True
 while running:
-    
-    # draws background image
-    screen.blit(background, (0, 0)) 
 
     # loop that quits the game cleanly
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    # draws background image
+    screen.blit(background, (0, 0)) 
 
     # updates the background (for the image)
     pygame.display.flip()
